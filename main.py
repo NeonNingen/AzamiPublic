793657e377f23bc2e5ad9754fb8c5664edf718ae
NeonNingen <Zaincheema1@aol.com>
2020-06-19 14:25:25 +0100

    V1.0 Azami Public Release Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from events import *
+
+main()
+
+'''
+Public Release V1.0 AzamiDB
+This is the first build of Azami and contains all the cogs I made before V2.0
+V2.0 will be released when Azami V3.0 is made and the date is TBD
+Note: This is old code so a lot of things here may be inefficient or wasteful. 
+All this code was changed in 2.0 and was written better, cleaner and way more efficient
+You can go ahead and reprogram it if you want and manipulate it to your heart content!
+Or just use it as it is, as it does work.
+P.S Some code was changed a little for this release.
+
+Installing Steps - Read the readme.md
+The cogs the bot has:
+basic.py
+dnd.py
+fun.py
+help.py
+jojo.py
+maths.py
+mod.py
+owner.py
+server.py
+
+Thanks for reading this extra info. Have fun programmers!
+'''
