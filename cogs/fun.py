8f38935c239101c1db63af18c04260f30188c806
Epicyon-H <64486098+Epicyon-H@users.noreply.github.com>
2020-07-01 21:35:20 +0100

    Update fun.py
    Improved the rps command

--- cogs/fun.py
+++ cogs/fun.py
@@ -84,145 +84,109 @@ class Fun(commands.Cog):
 	@commands.command(description="Let's play a game of Jan Ken Pon!",
 					  aliases=['rockpaperscissors', 'rock'])
 	async def rps(self, ctx):
+		choices = {'r' : 
+						{"name" : "Rock",
+						"compImage" : "https://technabob.com/blog/wp-content/uploads/2018/07/ksts_st_rock_mood_light_colors.gif", 
+						"userImage" : "https://media.giphy.com/media/26gsdS1KCyxwTl6IU/giphy.gif", 
+						"beats" : "s",
+						"message" : "You threw out a rock",
+						"colour" : discord.Color.greyple()},
+					'p' : 
+						{"name" : "Paper",
+						"compImage" : "https://share.gifyoutube.com/yEMl0X.gif", 
+						"userImage" : "https://media.giphy.com/media/l3q2RoEgmvxXIvA5i/giphy.gif", 
+						"beats" : "r",
+						"message" : "You hit me with paper",
+						"colour" : discord.Color.teal()},
+					's' : 
+						{"name" : "Scissors",
+						"compImage" : "https://66.media.tumblr.com/2037d7aa49c718d3229c14eddade2837/tumblr_ov7tlr0UzF1vz54q7o3_500.gifv", 
+						"userImage" : "https://media1.giphy.com/media/jcBj1VbbrXgJy/source.gif", 
+						"beats" : "p",
+						"message" : "You slashed out some scissors",
+						"colour" : discord.Color.red()}
+					}
+
 		hello_em = discord.Embed(title=f"Hello {ctx.message.author.name}",
 								 description="Let's play Rock, paper, scissors!",
 								 color=discord.Color.green())
-		hello_em.set_image(url='https://media1.giphy.com/media/xUOwFX9O1080yxFDk4/source.gif')
+		hello_em.set_image(url="https://media1.giphy.com/media/xUOwFX9O1080yxFDk4/source.gif")
 		hello_em.set_thumbnail(url=ctx.message.author.avatar_url)
 		hello_em.set_footer(text="Your opponent is me!",
-							   icon_url=self.azami.user.avatar_url)
+							   icon_url=bot.user.avatar_url)
 		msg = await ctx.send(embed=hello_em)
 
 
-		wins = 0
-		losses = 0
-		ties = 0
+		inputChoice, wins, losses, ties = '', 0, 0, 0
 
-		user_rock_em = discord.Embed(title="You threw out a rock",
-									 description="Rock against...",
-									 color=discord.Color.greyple())
-		user_rock_em.set_image(url="https://media.giphy.com/media/26gsdS1KCyxwTl6IU/giphy.gif")
-		user_rock_em.set_thumbnail(url=ctx.message.author.avatar_url)
-
-		user_pap_em = discord.Embed(title="You hit me with paper",
-									 description="Paper against...",
-									 color=discord.Color.teal())
-		user_pap_em.set_image(url="https://media.giphy.com/media/l3q2RoEgmvxXIvA5i/giphy.gif")
-		user_pap_em.set_thumbnail(url=ctx.message.author.avatar_url)
-		
-		user_sci_em = discord.Embed(title="You slashed out some scissors",
-									 description="Scissors against...",
-									 color=discord.Color.red())
-		user_sci_em.set_image(url="https://media1.giphy.com/media/jcBj1VbbrXgJy/source.gif")
-		user_sci_em.set_thumbnail(url=ctx.message.author.avatar_url) 
-
-		comp_rock_em = discord.Embed(title="Rock!",
-									 color=discord.Color.greyple())
-		comp_rock_em.set_image(url="https://technabob.com/blog/wp-content/uploads/2018/07/ksts_st_rock_mood_light_colors.gif")
-		comp_rock_em.set_thumbnail(url=self.azami.user.avatar_url)
-
-		comp_pap_em = discord.Embed(title="Paper!",
-									 color=discord.Color.teal())
-		comp_pap_em.set_image(url="https://share.gifyoutube.com/yEMl0X.gif")
-		comp_pap_em.set_thumbnail(url=self.azami.user.avatar_url)
-
-		comp_sci_em = discord.Embed(title="Scissors!",
-									 color=discord.Color.red())
-		comp_sci_em.set_image(url="https://66.media.tumblr.com/2037d7aa49c718d3229c14eddade2837/tumblr_ov7tlr0UzF1vz54q7o3_500.gifv")
-		comp_sci_em.set_thumbnail(url=self.azami.user.avatar_url)
-
-		you_win_em = discord.Embed(title="Congrats you win!",
-									 description="I'll get you next time!",
-									 color=discord.Color.gold())
-		you_win_em.set_image(url="https://media.giphy.com/media/l0HlMWVJqvf86klnq/giphy.gif")
-		you_win_em.set_thumbnail(url=ctx.message.author.avatar_url)
-
-		you_lose_em = discord.Embed(title="Better luck next time!",
-									 description="Ha! You lost!",
-									 color=discord.Color.dark_red())
-		you_lose_em.set_image(url="https://media.giphy.com/media/5MtOIdkHhxPFu/giphy.gif")
-		you_lose_em.set_thumbnail(url=self.azami.user.avatar_url)
-
-		tie_em = discord.Embed(title="It's a tie...",
-									 description="Prepare to lose next time!",
-									 color=discord.Color.purple())
-		tie_em.set_image(url="https://i.makeagif.com/media/2-03-2016/2VqsWK.gif")
-		tie_em.set_thumbnail(url=ctx.message.author.avatar_url)
+		def check(c):
+			return c.author == ctx.author and c.content.lower() in ['r', 'p', 's', 'q']
 
 		while True:
 			await ctx.send(f'Wins: {wins}, Losses: {losses}, Ties: {ties}', delete_after=5)
-			while True:
-				msg2 = await ctx.send("What do you choose (r)ock, (p)aper, (s)cissors or (q)uit")
-				player = await self.azami.wait_for('message')
-				if player.content == 'q':
-					await ctx.send("See you next time")
-					return
-				if player.content == 'r' or player.content == 'p' or player.content == 's':
-					break
-				await ctx.send('Write r, p, s or q!', delete_after=5)
+			choiceMessage = await ctx.send("What do you choose (r)ock, (p)aper, (s)cissors or (q)uit")
+			try:
+				inputChoice = await bot.wait_for('message', check=check, timeout=20)
+			except:
+				await ctx.send('You took too long to respond.')
+				return
+			inputChoice = inputChoice.content
+			choiceMessage.delete()
 
-			if player.content == 'r':
-				await player.delete()
-				await msg2.delete()
-				await msg.edit(embed=user_rock_em)
-			elif player.content == 'p':
-				await player.delete()
-				await msg2.delete()
-				await msg.edit(embed=user_pap_em)
-			elif player.content == 's':
-				await player.delete()
-				await msg2.delete()
-				await msg.edit(embed=user_sci_em)
+			if inputChoice == 'q':
+				await ctx.send("See you next time")
+				return
 
-			randomnum = randint(1, 3)
-			if randomnum == 1:
-				computer = 'r'
-				await sleep(3)
-				await msg.edit(embed=comp_rock_em)
-			elif randomnum == 2:
-				computer = 'p'
-				await sleep(3)
-				await msg.edit(embed=comp_pap_em)
-			elif randomnum == 3:
-				computer = 's'
-				await sleep(3)
-				await msg.edit(embed=comp_sci_em)
+			userChoice = choices[inputChoice.lower()]
+			userEmbed = discord.Embed(title=userChoice["message"], 
+									  description=f'{userChoice["name"]} against...', 
+									  color=userChoice["colour"])
+			userEmbed.set_image(url=userChoice["userImage"])
+			userEmbed.set_thumbnail(url=ctx.message.author.avatar_url)
+			await msg.edit(embed=userEmbed)
 
-			if player.content == computer:
-				await ctx.send("Please wait...", delete_after=4)
-				await sleep(4)
-				await msg.edit(embed=tie_em)
+			randrps = choice(['r', 'p', 's'])
+			compChoice = choices[randrps]
+			compEmbed = discord.Embed(title=f'{compChoice["name"]}!', 
+									  color=compChoice["colour"])
+			compEmbed.set_image(url=compChoice["compImage"])
+			compEmbed.set_thumbnail(url=bot.user.avatar_url)
+
+			await asyncio.sleep(3)
+			await msg.edit(embed=compEmbed)
+
+			await ctx.send("Please wait...", delete_after=4)
+
+			print(inputChoice)
+			print(randrps)
+
+			if inputChoice == randrps:
+				embed = discord.Embed(title="It's a tie...",
+									 description="Prepare to lose next time!",
+									 color=discord.Color.purple())
+				embed.set_image(url="https://i.makeagif.com/media/2-03-2016/2VqsWK.gif")
+				embed.set_thumbnail(url=ctx.message.author.avatar_url)
 				ties += 1
-			elif player.content == 'r' and computer == 's':
-				await ctx.send("Please wait...", delete_after=4)
-				await sleep(4)
-				await msg.edit(embed=you_win_em)
+
+			elif userChoice["beats"] == randrps:
+				embed = discord.Embed(title="Congrats you win!",
+									 description="I'll get you next time!",
+									 color=discord.Color.gold())
+				embed.set_image(url="https://media.giphy.com/media/l0HlMWVJqvf86klnq/giphy.gif")
+				embed.set_thumbnail(url=ctx.message.author.avatar_url)
 				wins += 1
-			elif player.content == 'r' and computer == 'p':
-				await ctx.send("Please wait...", delete_after=4)
-				await sleep(4)
-				await msg.edit(embed=you_lose_em)
+
+			else:
+				embed = discord.Embed(title="Better luck next time!",
+									 description="Ha! You lost!",
+									 color=discord.Color.dark_red())
+				embed.set_image(url="https://media.giphy.com/media/5MtOIdkHhxPFu/giphy.gif")
+				embed.set_thumbnail(url=bot.user.avatar_url)
 				losses += 1
-			elif player.content == 'p' and computer == 'r':
-				await ctx.send("Please wait...", delete_after=4)
-				await sleep(4)
-				await msg.edit(embed=you_win_em)
-				wins += 1
-			elif player.content == 'p' and computer == 's':
-				await ctx.send("Please wait...", delete_after=4)
-				await sleep(4)
-				await msg.edit(embed=you_lose_em)
-				losses += 1
-			elif player.content == 's' and computer == 'p':
-				await ctx.send("Please wait...", delete_after=4)
-				await sleep(4)
-				await msg.edit(embed=you_win_em)
-				wins += 1
-			elif player.content == 's' and computer == 'r':
-				await ctx.send("Please wait...", delete_after=4)
-				await sleep(4)
-				await msg.edit(embed=you_lose_em)
-				losses += 1
+			
+			await asyncio.sleep(4)
+			await msg.edit(embed=embed)
+
 
 
 	@blame.error
