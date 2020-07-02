e3d3cb35951bfbe88ff8391927759bae793cee44
Epicyon-H <64486098+Epicyon-H@users.noreply.github.com>
2020-07-02 05:32:07 +0100

    Update maths.py
    Makes it strip the floats of empty decimals, could make it slower but oh well

--- cogs/maths.py
+++ cogs/maths.py
@@ -9,26 +9,38 @@ class Maths(commands.Cog):
 	@commands.command(aliases = ['addition', 'plus', '+'], 
 					  description='9 + 10 = ?',
 					  usage='Adding two numbers together')
-	async def add(self, ctx, a: float, b: float):
-		await ctx.send(f"{a} + {b} = {a + b}")
+	async def add(self, ctx, a, b, *):
+		sum = float(a) + float(b)
+		if str(sum)[-2:] == '.0':
+			sum = str(sum)[:-2]
+		await ctx.send(a, "+", b, "=", sum)
 
 	@commands.command(aliases = ['takeaway', 'minus', '-'], 
 					  description='9 - 11 = ?',
 					  usage='Subtracting two numbers from each other')
-	async def subtract(self, ctx, a: float, b: float):
-		await ctx.send(f"{a} - {b} = {a - b}")
+	async def subtract(self, ctx, a, b, *):
+		sum = float(a) - float(b)
+		if str(sum)[-2:] == '.0':
+			sum = str(sum)[:-2]
+		await ctx.send(a, "-", b, "=", sum)
 
 	@commands.command(aliases = ['times', '*'], 
 					  description='9 x 11 = ?',
 					  usage='Multiply two numbers together')
-	async def multiply(self, ctx, a: float, b: float):
-		await ctx.send(a * b)
+	async def multiply(self, ctx, a, b, *):
+		sum = float(a) * float(b)
+		if str(sum)[-2:] == '.0':
+			sum = str(sum)[:-2]
+		await ctx.send(a, "*", b, "=", sum)
 
 	@commands.command(aliases = ['division', '/'], 
 					  description='9 / 11 = ?',
 					  usage='Divide two numbers from each other')
-	async def divide(self, ctx, a: float, b: float):
-		await ctx.send(a / b)
+	async def divide(self, ctx, a, b, *):
+		sum = float(a) / float(b)
+		if str(sum)[-2:] == '.0':
+			sum = str(sum)[:-2]
+		await ctx.send(a, "/", b, "=", sum)
 	
 
 def setup(azami):
