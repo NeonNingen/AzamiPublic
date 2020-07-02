8de5bf60509c3b67c42957dc65a24406e677314d
Epicyon-H <64486098+Epicyon-H@users.noreply.github.com>
2020-07-02 04:58:05 +0100

    Update __init__.py

--- randomimg/__init__.py
+++ randomimg/__init__.py
@@ -15,25 +15,19 @@ def slap():
 	return choice(slap_list)
 
 def shoot(num: int):
-	if num == 1:
-		gif_list = [
-		"http://i.imgur.com/hPL5TGD.gif",
-		"https://data.whicdn.com/images/207272964/original.gif"
-		]
-		return choice(gif_list)
-	elif num == 2:
-		gif_list = [
-		"https://media.giphy.com/media/5xaOcLAo1Gg0oRgBz0Y/giphy.gif",
-		"https://i.kym-cdn.com/photos/images/original/000/801/745/825.gif"
-		] # Finish these
-		return choice(gif_list)
-	elif num == 3:
-		gif_list = [
-		"https://s-media-cache-ak0.pinimg.com/originals/2d/fa/a9/2dfaa995a09d81a07cad24d3ce18e011.gif",
-		"https://i.imgur.com/W3MY1.gif",
-		"https://media.giphy.com/media/VuKsgBXgjOUtW/giphy.gif"
-		]
-		return choice(gif_list)
+	images = {1 : [
+			"http://i.imgur.com/hPL5TGD.gif",
+			"https://data.whicdn.com/images/207272964/original.gif"
+		],
+		 2 : [
+			"https://media.giphy.com/media/5xaOcLAo1Gg0oRgBz0Y/giphy.gif",
+			"https://i.kym-cdn.com/photos/images/original/000/801/745/825.gif"
+		],
+		 3 : [
+			"https://media.giphy.com/media/5xaOcLAo1Gg0oRgBz0Y/giphy.gif",
+			"https://i.kym-cdn.com/photos/images/original/000/801/745/825.gif"
+		]}
+	return images[num]
 
 		
 
