6f589146992055c435f5708cc8df5080356df0c0
NeonNingen <zaincheema1@aol.com>
2020-07-04 20:33:37 +0100

    Azami 1.0

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ V2.0 will be released when Azami V3.0 is made and the date is TBD
 Note: This is old code so a lot of things here may be inefficient or wasteful. 
 All this code was changed in 2.0 and was written better, cleaner and way more efficient
 You can go ahead and reprogram it if you want and manipulate it to your heart content!
-Or just use it as it is, as it does work.
+Or just use it as it is, as it does work. All changes have to be local and will not replace
+the github version.
 P.S Some code was changed a little for this release.
 
 Installing Steps - Read the readme.md
